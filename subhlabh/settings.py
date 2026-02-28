8273e19479f6b84810b392b7a40e8d7151269865
kaloiyap7177-stack <kaloiyap7177@gmail.com>
2026-02-28 13:58:50 +0530

    Remove MySQL, setup SQLite as database

--- subhlabh/settings.py
+++ subhlabh/settings.py
@@ -91,16 +91,8 @@ WSGI_APPLICATION = 'subhlabh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
